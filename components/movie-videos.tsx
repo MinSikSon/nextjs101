9c9a2a5c32a838c3e347c7cbe9227ffdda1020d1
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2024-06-22 23:00:23 +0900

    comment

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,7 +3,7 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
     console.log(`Fetching videos:${Date.now()}, id:${id}`);
     await new Promise((resolve) => setTimeout(resolve, 3000));
-    throw new Error("something broke...");
+    // throw new Error("something broke...");
     const res = await fetch(`${API_URL}/api/v1/tickers/${id}`);
     return res.json();
 }
